9a0c6e94543565ec0b34ed67e627906f3deac6b4
lucasggimenez <lucasggimenez@usp.br>
2020-12-19 11:56:51 -0300

    v0

---
+++ main.py
@@ -0,0 +1,16 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+print(sys.version)
+
+
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+
+r = requests.get('https://coreyms.com')
+print(r.status_code)
